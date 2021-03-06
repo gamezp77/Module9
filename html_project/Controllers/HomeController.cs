aa46fbde057d6da995c0b9f3ef75ab49ec9901a7
Pablo Gamez Jr <>
2021-03-06 00:04:10 -0700

    MidTerm

--- html_project/Controllers/HomeController.cs
+++ html_project/Controllers/HomeController.cs
@@ -38,7 +38,11 @@ namespace html_project.Controllers
                 " and the Passoword is:" + Password;
             return Content(myResults);
         }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
